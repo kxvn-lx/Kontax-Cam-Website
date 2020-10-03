b6e152a6ae2631a86aa6ea3a4d195c49ebe73bab
Kevin Laminto <kevin.laminto@gmail.com>
2020-10-04 01:08:30 +1000

    Added kontax editor

--- src/components/showcaseView.js
+++ src/components/showcaseView.js
@@ -4,19 +4,33 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import ImageShowcase from './imageShowcase'
 
-export default function showcaseView(props) {
-    const isLeftAligned = props.isLeftAligned
-    const imageName = props.imageName
-    const title = props.title
-    const description = props.description
+export default function showcaseView() {
     return (
         <Fade>
-            <ShowcaseWrapper isLeftAligned={isLeftAligned} className="grid md:grid-cols-2 grid-cols-1 gap-4 my-20">
+            <ShowcaseWrapper isLeftAligned={true} className="grid md:grid-cols-2 grid-cols-1 gap-4 my-20">
                 <div id="content" className="m-auto">
-                    <h1 className="font-bold text-2xl md:text-5xl">{title}</h1>
-                    <p className={`${isLeftAligned ? `md:float-left` : `md:float-right`} mx-auto w-5/6`}>{description}</p>
+                    <span className="text-orange-500 text-sm">Coming soon</span>
+                    <h1 className="font-bold text-2xl md:text-5xl">Kontax Editor</h1>
+                    <p id="description" className={`mx-auto w-5/6`}>Import your own photo taken outside the app and edit it with Kontax Cam's very own filters and effects.</p>
                 </div>
-                <ImageShowcase imageName={imageName} className="w-full md:w-3/4 mx-auto" />
+                <ImageShowcase imageName="004.png" className="w-full md:w-3/4 mx-auto" />
+            </ShowcaseWrapper>
+            <ShowcaseWrapper isLeftAligned={false} className="grid md:grid-cols-2 grid-cols-1 gap-4 my-20">
+                <div id="content" className="m-auto">
+                    <h1 className="font-bold text-2xl md:text-5xl">Handpicked premium filters</h1>
+                    <p id="description" className={`mx-auto w-5/6`}>Kontax Cam provides filter collections that are made from emulating both classic film stocks and digital camera artifacts.
+            Experiment with different combinations to create your own filter and effects combo!</p>
+                </div>
+                <ImageShowcase imageName="003.png" className="w-full md:w-3/4 mx-auto" />
+            </ShowcaseWrapper>
+
+            <ShowcaseWrapper isLeftAligned={true} className="grid md:grid-cols-2 grid-cols-1 gap-4 my-20">
+                <div id="content" className="m-auto">
+                    <h1 className="font-bold text-2xl md:text-5xl">Carefully crafted effects</h1>
+                    <p id="description" className={`mx-auto w-5/6`}>Kontax Cam believes that every photographer should define their own style of art. With that in mind,
+            Kontax Cam delivers a handful amount of effects to make your photo looks unique.</p>
+                </div>
+                <ImageShowcase imageName="002.png" className="w-full md:w-3/4 mx-auto" />
             </ShowcaseWrapper>
         </Fade>
     )
@@ -38,6 +52,11 @@ const ShowcaseWrapper = styled.div`
             text-align: center !important;
             order: 0 !important;
             margin-bottom: 2rem;
+        }
+
+        #description {
+            float: ${props => props.isLeftAligned ? 'left' : 'right'};
+            display: contents;
         }
     }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -56,16 +56,7 @@ function IndexPage() {
             <div className="separator" />
 
             <div className="mt-10">
-                {
-                    showcaseList.map(item => (
-                        <ShowcaseView key={item.title}
-                            title={item.title}
-                            imageName={item.imageName}
-                            isLeftAligned={item.isLeftAligned}
-                            description={item.description}
-                        />
-                    ))
-                }
+                <ShowcaseView />
             </div>
 
             <div className="separator" />
@@ -106,79 +97,49 @@ export default IndexPage;
 // Styled Components
 
 // Datasource
-const showcaseList = (
-    [
-        {
-            isLeftAligned: false,
-            imageName: '003.png',
-            title: 'Handpicked premium filters',
-            description: `Kontax Cam provides filter collections that are made from emulating both classic film stocks and digital camera artifacts. 
-            Experiment with different combinations to create your own filter and effects combo!`
-        },
-        {
-            isLeftAligned: true,
-            imageName: '002.png',
-            title: 'Carefully crafted effects',
-            description: `Kontax Cam believes that every photographer should define their own style of art. With that in mind,
-            Kontax Cam delivers a handful amount of effects to make your photo looks unique.`
-        },
-        {
-            isLeftAligned: false,
-            imageName: '004.png',
-            title: 'One lab to rule them all',
-            description: `Your very own, privacy first, images lab. Just like the iconic darkroom for film photographers,
-            we provide you with Lab - the place for all of your masterpiece.`
-        },
-    ]
-)
-
 const featureList = (
     [
         {
-            iconName: 'aperture.svg',
             title: 'Multiple lens support',
             description: `If your iPhone supports the telephoto or wide-angle lens, Kontax Cam is smart enough to enable you to utilise them to its extend.`
         },
         {
-            iconName: 'happy-face.svg',
             title: 'Simple and minimal',
             description: `Kontax Cam is built around minimalism and simplicity design. 
             That way, we can ensure that the user's experience and photo creation will always be the headline.`
         },
         {
-            iconName: 'copy.svg',
             title: 'Multi action',
             description: `The lab is powerful enough to provides multiple save and delete of images. Lesser time organising your photos, more time taking your masterpiece.`
         },
         {
-            iconName: 'human.svg',
             title: 'Gestured-based',
             description: `Kontax Cam is designed for human. Hence your masterpiece - is just but a tap and swipe away.`
         },
         {
-            iconName: 'widget.svg',
             title: 'iOS 14 Widget',
             description: `Kontax Cam supports iOS 14 widgets by showcasing your beautiful masterpiece into your homescreen, directly from your lab.`
         },
         {
-            iconName: 'volume.svg',
             title: 'Volume button trigger',
             description: `Tired of pressing the big ol' shutter button? Kontax Cam allows you to snap your masterpiece by just pressing the volume up/down button.`
         },
         {
-            iconName: 'sun.svg',
             title: 'Light/Dark mode',
             description: `By default Kontax Cam follows the system's appearance. However, you can pick either a light or dark mode in the settings page!`
         },
         {
-            iconName: 'drag.svg',
             title: 'Your own adjustment choice',
             description: `Adjust the exposure with a swipe up or down, or simply just tap anywhere on the camera screen to auto focus/exposure.`
         },
         {
-            iconName: 'shake.svg',
             title: 'Shake to report the bug!',
             description: `Spot a bug? Shake it so that we could squash them for ya!`
+        },
+        {
+            title: 'One lab to rule them all',
+            description: `Your very own, privacy first, images lab. Just like the iconic darkroom for film photographers, we provide you with Lab - the place for all of your masterpiece.
+            `
         }
     ]
 )
